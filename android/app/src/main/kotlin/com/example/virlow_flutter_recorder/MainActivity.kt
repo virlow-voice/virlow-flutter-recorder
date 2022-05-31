db94d6fff4bae14ae298e6594340a9a713acbfae
Rui Costa <hello@virlow.com>
2022-05-31 17:54:03 -0400

    Revert "First Commit"
    This reverts commit 142dd9dc306558bd000a0d1b1d0380a9bc5d7872.

--- android/app/src/main/kotlin/com/example/virlow_flutter_recorder/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.virlow_flutter_recorder
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- build/ios/Debug-iphonesimulator/gallery_saver/gallery_saver.framework/Headers/GallerySaverPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface GallerySaverPlugin : NSObject<FlutterPlugin>
-@end
